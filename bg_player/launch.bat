721d0f14d5ccf6d1962a6258b6d54b91c77c6237
LJS <js@JSui-MacBookPro.local>
2026-02-22 22:17:05 +0900

    Add bg_player and bg_server-multi from BEER3

---
+++ bg_player/launch.bat
@@ -0,0 +1,1 @@
+start firefoxportable beergame-welcome.htm
---
+++ bg_server-multi/Beergame_Server_START.bat
@@ -0,0 +1,27 @@
+@echo off
+
+cd jdk
+
+echo
+echo setting BEERGAME_JAVA_HOME and BEERGAME_JRE_HOME for Beergame USB-Stick
+set BEERGAME_JAVA_HOME=%cd%
+set BEERGAME_JRE_HOME=%cd%
+echo to %BEERGAME_JAVA_HOME%
+
+cd ..
+cd tomcat
+cd webapps
+cd BeergameProject
+cd beergame
+
+set BEERGAME_PATH=%cd%
+echo setting BEERGAME_PATH to %BEERGAME_PATH%
+
+set JAVA_OPTS=%JAVA_OPTS% -DBEERGAME_PATH=%BEERGAME_PATH%
+echo setting JAVA_OPTS to %JAVA_OPTS%
+
+cd ..
+cd ..
+cd ..
+cd bin
+startup.bat
